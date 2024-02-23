0b35d0bd8264d03906e92bba5983b8fcc229c044
Peter Kabiri <peter.kabiri@gmail.com>
2024-02-23 19:20:55 +0100

    Added pyshark to tests

--- tests/agent_advert.py
+++ tests/agent_advert.py
@@ -2,8 +2,9 @@
 import subprocess
 import re
 from subprocess import PIPE
-# import pyshark
+import pyshark
 import os
+import time
 
 class net_scan():
 
@@ -17,79 +18,132 @@ class net_scan():
  
     def step_1(self):
 
-        #try:
-        #    self._fa = Ssh(host=self.__ip1, port=22, user="admin", passwd="admin")
-        #except Exception as err:
-        #    self._test_reporting.add_actual_msg("Connecting to Foreign Agent VM with IP %s failed with error %s" % (self.__ip1, err))
-        #    return False
 
-        #try:
-        #    self._ma = Ssh(host=self.__ip2, port=22, user="admin", passwd="admin")
-        #except Exception as err:
-        #    self._test_reporting.add_actual_msg("Connecting to Mobile Agent VM with IP %s failed with error %s" % (self.__ip1, err))
-        #    return False
+        print("\nRunning Agent Advertisement Packet\n")
+
+        closed_port_list = list()
+        result_list = list()
 
 
-
-            print("\nRunning Agent Advertisement Packet\n")
-
-            closed_port_list = list()
-            result_list = list()
+        cmd_1 = "./mip/src/mip -m"
+        cmd_2 = "./mip/src/mip -r"
 
 
-            cmd_1 = "./mip/src/mip -m"
-            cmd_2 = "./mip/src/mip -r"
-
-
-
+        try:
 
 
             aa_process = subprocess.Popen(['ssh','-tt', self.__ip1],
-                                   stdin=subprocess.PIPE, 
-                                   stdout = subprocess.PIPE,
-                                   universal_newlines=True,
+                                    stdin=subprocess.PIPE, 
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True,
                                 bufsize=0)
+            print("\nRunning Agent ddddddd Packet\n")
 
+
+        except Exception as err:
+            self._test_reporting.add_actual_msg("Connecting to Foriegn Agent VM with IP %s failed with error %s" % (self.__ip1, err))
+            return False
+
+        try:
             ma_process = subprocess.Popen(['ssh','-tt', self.__ip2],
                                    stdin=subprocess.PIPE, 
                                    stdout = subprocess.PIPE,
                                    universal_newlines=True,
                                 bufsize=0)
+            print("\nRunning Agent zzzz Packet\n")
+
+        except Exception as err:
+            self._test_reporting.add_actual_msg("Connecting to Mobile Agent VM with IP %s failed with error %s" % (self.__ip2, err))
+            return False
     
-            aa_process.stdin.write("echo 'admin' | sudo -S  ./mip/src/mip -m\n")
-            aa_process.stdin.write("ls .\n")
-            aa_process.stdin.write("echo END\n")
-            aa_process.stdin.write("uptime\n")
-            aa_process.stdin.write("logout\n")
-            aa_process.stdin.close()
+        aa_process.stdin.write("echo 'admin' | sudo -S  ./mip/src/mip -m\n")
 
-            ma_process.stdin.write("echo 'admin' | sudo -S  ./mip/src/mip -r\n")
-            ma_process.stdin.write("ls .\n")
-            ma_process.stdin.write("echo END\n")
-            ma_process.stdin.write("uptime\n")
-            ma_process.stdin.write("logout\n")
-            ma_process.stdin.close()
+        time.sleep(5)
 
-            for line in aa_process.stdout:
-                if line == "END\n":
-                    break
-                print(line,end="")
+        ma_process.stdin.write("echo 'admin' | sudo -S  ./mip/src/mip -r\n")
 
-            #to catch the lines up to logout
-            for line in  aa_process.stdout: 
-                print(line,end="")
+        ma_process.stdin.write("uptime\n")
+        time.sleep(30)
 
-            for line in ma_process.stdout:
-                if line == "END\n":
-                    break
-                print(line,end="")
 
-            #to catch the lines up to logout
-            for line in  ma_process.stdout: 
-                print(line,end="")
-        
-            aa_process.kill()
-            ma_process.kill()
+
+        state = self.check_packet_header(aa_process)
+
+        ma_process.kill()
+        aa_process.kill()
+        return state
+
+
+    def check_packet_header(self, aa_process):
+        """
+        Check IP packet header
+        """
+        time.sleep(10)
+        state = list()
+        # waiting for test center to start
+
+
+        path_capture_file = self._environment.get_project_folder()
+
+        self.__nodeA.get_linux_shell().send_cmd(["tcpdump -i tdma0.1 -c 1 -w p2ptcpdumpfile.pcap"])
+        ma_process.stdin.write("echo 'admin' | sudo -S  tcpdump -i enp0s3 -c 1 -w agent_adv.pcap\n")
+
+        # get local copy of dumpfile.pcap
+
+
+         # ma_process.get_scp().scp_get(remotefile="agent_adv.pcap", localpath=path_capture_file)
+
+        # remove temporary dumpfile.pcap on linux machine
+        ma_process.stdin.write("rm agent_adv.pcap\n")
+
+
+
+        # read pcap file and read the tos vamolue
+        pcap_file = pyshark.FileCapture(os.path.join(self._environment.get_project_folder(), 'p2ptcpdumpfile.pcap'))
+        try:
+            for packet in pcap_file:
+                tos_hex_value = int(packet.layers[1].dsfield, 16)
+                gre_key_value = int(packet.layers[2].key, 16)
+
+                if int(tos_hex_value) == self.__dscp_service_value_list[self.__dscp_service]:
+
+                    if gre_key_value == self.__tun1_key:
+                        print("\nAgent advert message is sent to all multicats IP address and it is received b Mobile Node\n")
+                        state.append(True)
+                    else:
+                        print("\nAgent advert message is Not sent to all multicats IP address\n")
+
+                        state.append(False)
+
+                    if gre_key_value == self.__tun1_key:
+                        print("\nAgent advert message is sent to all multicats IP address with the right Code\n")
+                        state.append(True)
+                    else:
+                        print("\nAgent advert message is Not sent to all multicats IP address\n")
+
+                        state.append(False)
+
+                    if gre_key_value == self.__tun1_key:
+                        print("\nAgent advert message is sent to all multicats IP address with the right Typse\n")
+                        state.append(True)
+                    else:
+                        print("\nAgent advert message is Not sent to all multicats IP address\n")
+
+                        state.append(False)
+
+
+                else:
+                    print("Failed to interpret captured packet on Global VRF")
+                    state.append(True)
+
+
+        except Exception, err:
+            print("Failed to  captured packet with error %s" % err)
+
+            state.append(False)
+
+        return all(state) if state else False
+    
 
 net_scan().step_1()
 
