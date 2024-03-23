d425ff6495ea83f709b63219d8cddb112f9c0120
Dancer <Peter.Kabiri@gmail.com>
2024-03-23 12:47:51 +0100

    Tests: Loggind points in shell script

--- tests/configure_vm1.sh
+++ tests/configure_vm1.sh
@@ -4,10 +4,20 @@ echo "Hello, world!"
 ssh lubuntu@192.168.0.53 << EOF
 
 cd mip
+echo "aa!"
+
 echo $(pwd)
+echo "bbb!"
+
 git pull
+echo "ccc!"
+
 cd src
+echo "ddd!"
+
 echo $(pwd)
+echo "eee!"
+
 rm mip
 echo $(pwd)
 cd obj
