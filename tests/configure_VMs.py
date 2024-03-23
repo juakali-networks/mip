48c9857e85bae9c9735e6031ccc3fc0fba3134db
Dancer <Peter.Kabiri@gmail.com>
2024-03-23 19:04:35 +0100

    Renamed test runner. Adapted VM configuration to use python instead of shitty bash

---
+++ tests/configure_VMs.py
@@ -0,0 +1,115 @@
+
+import subprocess
+import re
+from subprocess import PIPE
+import pyshark
+import os
+import time
+import paramiko
+from scp.scp import SCPClient
+
+class setup_vm():
+
+    def __init__(self):
+
+        # Configs. Change your settings here
+        self._pwd = "lubuntu"
+        self._user_name = "lubuntu"
+
+ 
+ 
+    def step_1(self, vm_ip):
+     
+
+        print("\nConnecting to Virtual Machine with IP address %s\n" % vm_ip)
+       
+        vm_user = "%s@%s" % (self._user_name, vm_ip)
+
+        cmd = "cd mip && git pull"
+        vm_process = subprocess.Popen(['ssh','-tt', vm_user, "%s" % cmd],
+                                    stdin=subprocess.PIPE, 
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True,
+                                bufsize=0)
+        results_output, results_error = vm_process.communicate()
+        results_output_bytes = bytes(results_output, 'ascii')    
+        print(results_output_bytes)  
+
+        cmd = "rm mip/src/mip"
+        vm_process = subprocess.Popen(['ssh','-tt', vm_user, "%s" % cmd],
+                                    stdin=subprocess.PIPE, 
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True,
+                                bufsize=0)
+        results_output, results_error = vm_process.communicate()
+        results_output_bytes = bytes(results_output, 'ascii')    
+        print(results_output_bytes)  
+
+        cmd = "rm mip/src/obj/mip.o"     
+        vm_process = subprocess.Popen(['ssh','-tt', vm_user, "%s" % cmd],
+                                    stdin=subprocess.PIPE, 
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True,
+                                bufsize=0)
+        results_output, results_error = vm_process.communicate()
+        results_output_bytes = bytes(results_output, 'ascii')    
+        print(results_output_bytes)    
+
+        cmd = "cd mip/src && make clean"    
+        vm_process = subprocess.Popen(['ssh','-tt', vm_user, "%s" % cmd],
+                                    stdin=subprocess.PIPE, 
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True,
+                                bufsize=0)
+        results_output, results_error = vm_process.communicate()
+        results_output_bytes = bytes(results_output, 'ascii')    
+        print(results_output_bytes)
+
+        cmd = "cd mip/src && make"    
+        vm_process = subprocess.Popen(['ssh','-tt', vm_user, "%s" % cmd],
+                                    stdin=subprocess.PIPE, 
+                                    stdout = subprocess.PIPE,
+                                    universal_newlines=True,
+                                bufsize=0)
+        results_output, results_error = vm_process.communicate()
+        results_output_bytes = bytes(results_output, 'ascii')    
+        print(results_output_bytes)
+            
+        vm_process.kill()
+
+        return True
+
+ip1 = "192.168.0.34"
+ip2 = "192.168.0.53"
+
+setup_vm().step_1(ip1)
+setup_vm().step_1(ip2)
+
+    
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
