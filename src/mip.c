09f9d1032149704caec5b2a0dd96d0418513e78f
Dancer <Peter.Kabiri@gmail.com>
2024-03-24 13:22:01 +0100

    Added logging

--- src/mip.c
+++ src/mip.c
@@ -443,8 +443,8 @@ registration_request(int lft, int sockfd)
     			perror("socket() error");
     			exit(2);
   			}
-			logmsg(LOG_INFO, "Source address P %s\n", inet_ntoa(*(struct in_addr *)&(ip->saddr)));
-			logmsg(LOG_INFO, "Destination address p %s\n", inet_ntoa(*(struct in_addr *)&(ip->daddr)));
+			logmsg(LOG_INFO, "Source address %s\n", inet_ntoa(*(struct in_addr *)&(ip->saddr)));
+			logmsg(LOG_INFO, "Destination address %s\n", inet_ntoa(*(struct in_addr *)&(ip->daddr)));
 
 
       		addr.sin_family = AF_INET;
