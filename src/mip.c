1dbe2689724cb29c72c0c97da176433d55742fe3
Dancer <Peter.Kabiri@gmail.com>
2024-04-05 19:10:15 +0200

    Changed DEstination IP address for the packet

--- src/mip.c
+++ src/mip.c
@@ -489,12 +489,12 @@ registration_request(int lft, int sockfd)
 
       		addr.sin_family = AF_INET;
       		addr.sin_port = htons(434);
-      		addr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr *)&(ip->saddr)));
+      		// addr.sin_addr.s_addr = inet_addr(inet_ntoa(*(struct in_addr *)&(ip->saddr)));
 			//addr.sin_addr.s_addr = INADDR_ANY;
 			if (fa_reg_request)
-				addr.sin_addr.s_addr = inet_addr("192.168.0.85");
-			if (mn_reg_request)
 				addr.sin_addr.s_addr = inet_addr("192.168.0.240");
+			if (mn_reg_request)
+				addr.sin_addr.s_addr = inet_addr("192.168.0.34");
 
 			rreq->reg_req_type = ICMP_REGREQUEST;
 			rreq->flags = 0;
