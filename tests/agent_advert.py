d807b8331c342ae068715bebebb03264fe0cb687
Dancer <Peter.Kabiri@gmail.com>
2024-04-05 09:02:23 +0200

    Corrected VM IPs

--- tests/agent_advert.py
+++ tests/agent_advert.py
@@ -17,7 +17,7 @@ class agent_adv():
         self._user_name = "lubuntu"
         self._pwd = "lubuntu"
         self._ip1 = "192.168.0.34"
-        self._ip2 = "192.168.0.53"
+        self._ip2 = "192.168.0.240"
         self._all_host_mcast_addr = "224.0.0.1"
         self._agent_advert_type = "9"
         self._agent_advert_code = "16"
